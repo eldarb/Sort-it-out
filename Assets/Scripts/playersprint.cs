f91f5abe5560fd0e95b8492fd6a9a201dbeb77cb
Rakshaan Khan <55557308+rakshaankhan@users.noreply.github.com>
2024-02-20 01:00:08 -0500

    PlayerSprint.cs updated
    I fixed line 53 and got rid of the unnecessary line to still implement the script.

--- Assets/Scripts/playersprint.cs
+++ Assets/Scripts/playersprint.cs
@@ -50,7 +50,6 @@ public class PlayerSprint : MonoBehaviour
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             characterController.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
