913cad70740f169360bd213d8b5ffff62912aa64
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-02-22 16:13:02 -0800

    added coding standard summaries to PlayerLadderMovement script

--- Assets/PlayerLadderMovement.cs
+++ Assets/PlayerLadderMovement.cs
@@ -2,44 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+/// <summary>
+/// Allows player to climb ladders
+/// </summary>
 public class PlayerLadderMovement : MonoBehaviour
 {
+    /// <summary>
+    /// used to change movement variable in playerManager
+    /// </summary>
     PlayerManager playerManager;
-    PlayerInput playerInput;
-    InputAction climbAction;
+    /// <summary>
+    /// used to check if player is in contact with a ladder
+    /// </summary>
     public bool climbingLadder { private set; get; }
+    /// <summary>
+    /// rate the player climbs up ladders
+    /// </summary>
     [SerializeField] public int climbingLadderSpeed;
+    /// <summary>
+    /// sets the above variables to default values
+    /// </summary>
     private void Start()
     {
         playerManager = GetComponent<PlayerManager>();
-        playerInput = GetComponent<PlayerInput>();
-        climbAction = playerInput.actions.FindAction("Move");
         climbingLadder = false;
         climbingLadderSpeed = 4;
     }
 
+    /// <summary>
+    /// called when player triggers ladder collider
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         
         if(other.gameObject.CompareTag("Ladder"))
         {
-            // disable jumping when player is on ladder
             Debug.Log("On ladder");
             climbingLadder = true;
         }
     }
-
+    /// <summary>
+    /// called when player leaves ladder collider
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.CompareTag("Ladder"))
         {
-            // enable jumping when player is off ladder
             Debug.Log("Off ladder");
             climbingLadder = false;
         }
     }
 
+    /// <summary>
+    /// changes movement variable when player is climbing ladder, else movement does not change
+    /// </summary>
+    /// <param name="movement"></param>
+    /// <returns></returns>
     public Vector3 HandleLadderMovement(Vector3 movement)
     {
         if (climbingLadder)
